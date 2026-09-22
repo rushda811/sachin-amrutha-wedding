9a5f9bb244c98309e987f213c09bd0fe8dcec1d3
Fathima Rushda <fathimarushda811@gmail.com>
2026-09-22 18:12:49 +0530

    image

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -610,13 +610,14 @@ const [galleryOpen, setGalleryOpen] = useState(false);
   className="gallery-lightbox-grid"
   onClick={(event) => event.stopPropagation()}
 >
-  <img src={laughter} alt="Wedding photo" />
-  <img src={stationery} alt="Wedding stationery" />
-  <img src={detail} alt="Wedding details" />
-  <img src={bouquet} alt="Wedding bouquet" />
-  <img src={gardenWalk} alt="Wedding photo" />
-  <img src={walk} alt="Wedding photo" />
-  <img src={estate} alt="Wedding venue" />
+<img src={laughter} alt="Wedding photo" />
+<img src={stationery} alt="Wedding stationery" />
+<img src={detail} alt="Wedding details" />
+<img src={bouquet} alt="Wedding bouquet" />
+<img src={gardenWalk} alt="Wedding photo" />
+<img src={walk} alt="Wedding photo" />
+<img src={estate} alt="Wedding venue" />
+<img src={portrait} alt="Amrutha and Sachin" />
 </div>
   </div>
 )}
